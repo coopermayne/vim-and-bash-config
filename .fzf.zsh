90d4ed8edd1176d96a93473931dec92b880a9eaa
Cooper Mayne <coopermayne@gmail.com>
2020-05-07 23:51:33 -1000

    fzf

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,2 @@
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
-fi
-
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+bindkey -s '^v' 'nvim $(fzf)^M'
+bindkey -s '^n' 'cd ~/Documents/Notebook^Mnvim -c :FZF^M'
